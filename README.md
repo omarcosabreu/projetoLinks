c799805cd06d4cb62325b6374e9b8ec2c4563ede
Marcos Abreu <marcosmendes.abreu7@gmail.com>
2024-01-06 20:15:42 -0300

    update project

---
+++ README.md
@@ -0,0 +1,24 @@
+<h1 align="center">My Profile | Marcos Abreu</h1>
+
+<br>
+
+<p align="center">
+  <img alt="My Profile | Marcos Abreu" src="./assets/project.png" width="35%">
+  <img alt="My Profile | Marcos Abreu" src="./assets/project2.png" width="35%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Vite, ESlint
+- Node e NPM
+- Git e Github
+
+- [Acesse o projeto finalizado, online](https://omarcossabreu.github.io/project-Links/)
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT.
---
+++ main.js
@@ -0,0 +1,32 @@
+function toggleMode() {
+  const mode = document.documentElement
+  mode.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector(".profile img")
+
+  //substituir a imagem
+  if (mode.classList.contains("light")) {
+    img.setAttribute("src", "https://i.ibb.co/sVnQftm/IMG-2788.jpg")
+  } else {
+    img.setAttribute(
+      "src",
+      "https://avatars.githubusercontent.com/u/155205538?v=4"
+    )
+  }
+}
+
+
+//manipulação do HTML com JS
+let userProfile = document.querySelector("#userProfile")
+userProfile.innerHTML = (`
+  <header class="profile">
+        <img src="https://avatars.githubusercontent.com/u/155205538?v=4" alt="imagem de perfil"/>
+      <div>
+        <h1>Marcos  Abreu</h1><p>Desenvolvedor Front-End</p>
+      </div>
+  </header>
+`)
+
+let footer = document.querySelector("#footer")
+footer.innerHTML = ("<span>Marcos Abreu 2023 | Todos os direitos reservados</span>")
